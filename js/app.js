0d119a357838b23f0f57e122c6f230d2f3b66d32
Alex Fakhri <alexfakhri@me.com>
2016-09-20 14:48:57 +0100

    Using the colour sliders to create new RGB colours

--- js/app.js
+++ js/app.js
@@ -13,11 +13,20 @@ $(".controls li").click(function(){
 //When new color button is pressed
 $("#revealColorSelect").click(function(){
   //Show color Select or hide color Select
+  changeColor();
   $("#colorSelect").toggle();
 });
 
 //When color sliders change
+function changeColor() {
   //update the new colour span
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+};
+
+$("input[type=range]").on("input", changeColor);
 
 //When add colour is pressed
   //append the colour to the controls ul
